5026649a143c9ea51d74dea07c421617e03a2cc9
JorgeBeritan <inaikmcontacto@gmail.com>
2025-06-26 19:21:32 -0300

    Fix output msg

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -4,5 +4,5 @@ void main(List<String> args) {
    */
   var argsInt = int.parse(args[0]);
   assert(argsInt >= 80,'"Uncaught Error: Assertion failed: "The score must be bigger or equal to 80"');
-  print("You passed");
+  print("You Passed");
 }
